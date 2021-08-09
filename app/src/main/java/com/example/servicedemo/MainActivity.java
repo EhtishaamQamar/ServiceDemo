1ff17b8698edfde0ff52590320ec96dca01ee349
EhtishaamQamar <infernusrulz1@gmail.com>
2021-08-09 20:29:39 +0530

    Commit after merging Master & Experimental branch

--- app/src/main/java/com/example/servicedemo/MainActivity.java
+++ app/src/main/java/com/example/servicedemo/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Committing this on Master Branch");
-        System.out.println("Comitting this on Experimental Branch");
+        System.out.println("Committing this on Experimental Branch");
     }
 }
